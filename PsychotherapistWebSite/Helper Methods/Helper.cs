39dd203273c31ffa97b776d231a8267ce63098e1
mehmet <mhmtranas@gmail.com>
2017-10-01 18:26:19 +0300

    Added helper class

---
+++ PsychotherapistWebSite/Helper Methods/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PsychotherapistWebSite.Helper_Methods
+{
+    public class Helper
+    {
+        public static string Truncate(string text, byte value)
+        {
+            return text.Length > value ? text.Substring(0, value) : text;
+        }
+    }
+}
