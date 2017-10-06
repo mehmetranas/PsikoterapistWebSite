2845f52d56239ce152b33f3aeb4e3937003b2342
mehmet <mhmtranas@gmail.com>
2017-10-06 21:44:51 +0300

    Set Contact page and add message class

---
+++ PsychotherapistWebSite/Core/Repositories/IMessageRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using PsychotherapistWebSite.Core.Models;
+
+namespace PsychotherapistWebSite.Core.Repositories
+{
+    public interface IMessageRepository
+    {
+        void Add(Message message);
+        void Delete(int id);
+        void Read(int id);
+        IEnumerable<Message> GetMessages();
+        Message GetrMessage(int id);
+    }
+}
---
+++ PsychotherapistWebSite/Persistance/Repositories/MessageRepository.cs
@@ -0,0 +1,46 @@
+﻿using PsychotherapistWebSite.Core.Models;
+using PsychotherapistWebSite.Core.Repositories;
+using PsychotherapistWebSite.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PsychotherapistWebSite.Persistance.Repositories
+{
+    public class MessageRepository: IMessageRepository  
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MessageRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public void Add(Message message)
+        {
+            _context.Messages.Add(message);
+        }
+
+        public void Delete(int id)
+        {
+            var message = _context.Messages.FirstOrDefault(m => m.Id == id);
+            if (message != null) _context.Messages.Remove(message);
+        }
+
+        public void Read(int id)
+        {
+            var message = _context.Messages.FirstOrDefault(m => m.Id == id);
+            if (message != null) message.IsRead = true;
+        }
+
+        public IEnumerable<Message> GetMessages()
+        {
+            return _context.Messages.ToList();
+        }
+
+        public Message GetrMessage(int id)
+        {
+            return _context.Messages.FirstOrDefault(m => m.Id == id);
+        }
+    }
+}
